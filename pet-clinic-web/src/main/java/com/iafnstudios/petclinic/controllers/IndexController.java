be29cf7aa9d9010b5dd604de5b386d4dd338f96b
abdullahsen <sen-abdullah@yandex.com>
2018-08-31 03:30:51 +0300

    Adding Index Page and Controller. closes #8.

---
+++ pet-clinic-web/src/main/java/com/iafnstudios/petclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.iafnstudios.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public String getIndex(){
+
+        return "index";
+    }
+}
