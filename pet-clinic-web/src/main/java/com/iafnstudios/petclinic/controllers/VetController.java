e402d741f458d79f56ee7bfc6adc060543753dfb
abdullahsen <sen-abdullah@yandex.com>
2018-08-31 04:49:41 +0300

    Adding Vet List Page and Controller. closes #7.

---
+++ pet-clinic-web/src/main/java/com/iafnstudios/petclinic/controllers/VetController.java
@@ -0,0 +1,16 @@
+package com.iafnstudios.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+
+    @RequestMapping({"/vets", "/vets/", "/vets/index", "/vets/index.html"})
+    public String getVetList(){
+
+        return "vets/index";
+    }
+
+}
